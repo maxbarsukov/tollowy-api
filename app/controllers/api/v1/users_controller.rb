74be8b5e83c574b7f9f2fabc6369e0f918a90859
maxbarsukov <maximbarsukov@bk.ru>
2022-02-04 13:04:49 +0300

    feat: :sparkles: update users controller with serializers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,21 @@
 module Api
   module V1
     class UsersController < Api::V1::ApiController
+      before_action :set_user, only: %i[show]
+
       def index
         @users = User.all
-        render json: @users, status: :ok
+        render json: UserSerializer.new(@users), status: :ok
       end
 
       def show
+        render json: UserSerializer.new(@user), status: :ok
+      end
+
+      private
+
+      def set_user
         @user = User.find(params[:id])
-        render json: { success: true, data: { user: @user } }, status: :ok
       end
     end
   end
