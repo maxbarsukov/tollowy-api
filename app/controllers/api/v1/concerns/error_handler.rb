1d537261afdc771f52a2b8a4f418941e01ab0850
maxbarsukov <maximbarsukov@bk.ru>
2022-02-04 21:20:59 +0300

    style: :art: apply rubocop changes

--- app/controllers/api/v1/concerns/error_handler.rb
+++ app/controllers/api/v1/concerns/error_handler.rb
@@ -1,29 +1,35 @@
-module Api::V1::Concerns::ErrorHandler
-  extend ActiveSupport::Concern
+module Api
+  module V1
+    module Concerns
+      module ErrorHandler
+        extend ActiveSupport::Concern
 
-  included do
-    rescue_from ActionController::ParameterMissing do |exc|
-      error_unprocessable_entity(exc.message)
+        included do
+          rescue_from ActionController::ParameterMissing do |exc|
+            error_unprocessable_entity(exc.message)
+          end
+
+          rescue_from ActiveRecord::RecordInvalid do |exc|
+            error_unprocessable_entity(exc.message)
+          end
+
+          rescue_from ActiveRecord::RecordNotFound, with: :error_not_found
+
+          rescue_from Pundit::NotAuthorizedError, with: :error_unauthorized
+        end
+
+        def error_unprocessable_entity(message)
+          render json: { error: message, status: 422 }, status: :unprocessable_entity
+        end
+
+        def error_unauthorized
+          render json: { error: 'Unauthorized', status: 401 }, status: :unauthorized
+        end
+
+        def error_not_found
+          render json: { error: 'Not Found', status: 404 }, status: :not_found
+        end
+      end
     end
-
-    rescue_from ActiveRecord::RecordInvalid do |exc|
-      error_unprocessable_entity(exc.message)
-    end
-
-    rescue_from ActiveRecord::RecordNotFound, with: :error_not_found
-
-    rescue_from Pundit::NotAuthorizedError, with: :error_unauthorized
-  end
-
-  def error_unprocessable_entity(message)
-    render json: { error: message, status: 422 }, status: :unprocessable_entity
-  end
-
-  def error_unauthorized
-    render json: { error: 'Unauthorized', status: 401 }, status: :unauthorized
-  end
-
-  def error_not_found
-    render json: { error: 'Not Found', status: 404 }, status: :not_found
   end
 end
--- app/controllers/api/v1/concerns/response.rb
+++ app/controllers/api/v1/concerns/response.rb
@@ -1,7 +1,13 @@
-module Api::V1::Concerns::Response
-  extend ActiveSupport::Concern
+module Api
+  module V1
+    module Concerns
+      module Response
+        extend ActiveSupport::Concern
 
-  def json_response(object, status = :ok)
-    render json: object, status: status
+        def json_response(object, status = :ok)
+          render json: object, status: status
+        end
+      end
+    end
   end
 end
