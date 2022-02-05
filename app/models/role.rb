fdd4335e90fb3d43a16df0153a75549fb3b2dd19
maxbarsukov <maximbarsukov@bk.ru>
2022-02-05 11:05:58 +0300

    feat: :sparkles: seed basic roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,15 @@
 class Role < ApplicationRecord
+  ROLES = %w[
+    banned
+    warned
+    user
+    premium
+    primary
+    owner
+    moderator
+    admin
+  ].freeze
+
   has_and_belongs_to_many :users, join_table: :users_roles
 
   belongs_to :resource,
@@ -8,6 +19,9 @@ class Role < ApplicationRecord
   validates :resource_type,
             inclusion: { in: Rolify.resource_types },
             allow_nil: true
+
+  validates :name,
+            inclusion: { in: ROLES }
 
   scopify
 end
