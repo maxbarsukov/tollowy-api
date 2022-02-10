863ba3dbc57dd51f01a72f42a4f35d7ac550dc12
maxbarsukov <maximbarsukov@bk.ru>
2022-02-10 20:52:02 +0300

    feat: :sparkles: update error handler

--- app/controllers/api/v1/concerns/error_handler.rb
+++ app/controllers/api/v1/concerns/error_handler.rb
@@ -1,43 +1,38 @@
-module Api
-  module V1
-    module Concerns
-      module ErrorHandler
-        extend ActiveSupport::Concern
+module Api::V1::Concerns::ErrorHandler
+  extend ActiveSupport::Concern
 
-        included do
-          rescue_from ActionController::ParameterMissing, with: :render_unprocessable_entity
+  included do
+    rescue_from ActionController::ParameterMissing, with: :render_unprocessable_entity
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from JSON::ParserError, with: :render_bad_request
+    rescue_from Pundit::NotAuthorizedError, with: :render_unauthorized
+    rescue_from UnauthenticatedError, with: :render_unauthenticated
+  end
 
-          rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  def render_unprocessable_entity(exception)
+    render_error_response exception.message, :unprocessable_entity
+  end
 
-          rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  def render_not_found
+    render_error_response 'Not Found', :not_found
+  end
 
-          rescue_from JSON::ParserError, with: :render_bad_request
+  def render_bad_request(exception)
+    render_error_response exception.message, :bad_request
+  end
 
-          rescue_from Pundit::NotAuthorizedError, with: :render_unauthorized
-        end
+  def render_unauthorized
+    render_error_response 'Unauthorized', :unauthorized
+  end
 
-        def render_unprocessable_entity(exception)
-          render_error_response exception.message, :unprocessable_entity
-        end
+  def render_unauthenticated(exception)
+    render_error_response exception.message, :unauthorized
+  end
 
-        def render_not_found
-          render_error_response 'Not Found', :not_found
-        end
+  private
 
-        def render_bad_request(exception)
-          render_error_response exception.message, :bad_request
-        end
-
-        def render_unauthorized
-          render_error_response 'Unauthorized', :unauthorized
-        end
-
-        private
-
-        def render_error_response(message, status)
-          json_response({ error: message, status: Rack::Utils.status_code(status) }, status)
-        end
-      end
-    end
+  def render_error_response(message, status)
+    json_response({ error: message, status: Rack::Utils.status_code(status) }, status)
   end
 end
