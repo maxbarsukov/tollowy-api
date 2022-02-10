f31b0be492f8df02cc2c9b923b1e63e19bdb034e
maxbarsukov <maximbarsukov@bk.ru>
2022-02-10 19:01:51 +0300

    chore: :wrench: remove devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: :all, controllers: {
-    registrations: 'registrations',
-    sessions: 'sessions'
-  }
-
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       root 'home#index'
       resources :users, only: %i[index show]
-
-      devise_scope :user do
-        post   '/login'            => 'users/sessions#create',       as: :user_session
-        delete '/logout'           => 'users/sessions#destroy',      as: :destroy_user_session
-        post   '/signup'           => 'users/registrations#create',  as: :user_registration
-      end
     end
   end
 end
