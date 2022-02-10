f48bd71e82d83b2e25c4738455a78378cf5b9a15
maxbarsukov <maximbarsukov@bk.ru>
2022-02-10 20:53:38 +0300

    style: :art: change modules children style

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,11 +1,9 @@
-module Api
-  module V1
-    class HomeController < Api::V1::ApiController
-      before_action :authenticate_user!
+module Api::V1
+  class HomeController < Api::V1::ApiController
+    before_action :authenticate_user!
 
-      def index
-        json_response({ message: "If you see this, you're in!" })
-      end
+    def index
+      json_response({ message: "If you see this, you're in!" })
     end
   end
 end
