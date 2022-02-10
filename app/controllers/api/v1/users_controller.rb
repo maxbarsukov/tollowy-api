48d6cf47500d57599c253ec92bf9837645442d5b
maxbarsukov <maximbarsukov@bk.ru>
2022-02-10 20:53:50 +0300

    style: :art: change modules children style

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,20 @@
-module Api
-  module V1
-    class UsersController < Api::V1::ApiController
-      before_action :set_user, only: %i[show]
+module Api::V1
+  class UsersController < Api::V1::ApiController
+    before_action :set_user, only: %i[show]
 
-      def index
-        @users = User.all
-        json_response UserSerializer.new(@users)
-      end
+    def index
+      @users = User.all
+      json_response UserSerializer.new(@users)
+    end
 
-      def show
-        json_response UserSerializer.new(@user)
-      end
+    def show
+      json_response UserSerializer.new(@user)
+    end
 
-      private
+    private
 
-      def set_user
-        @user = User.find(params[:id])
-      end
+    def set_user
+      @user = User.find(params[:id])
     end
   end
 end
