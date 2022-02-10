c2299d40ca6eaf25f3fe2f476fc8de48ad3d82c7
maxbarsukov <maximbarsukov@bk.ru>
2022-02-10 20:52:29 +0300

    feat: :sparkles: create authenticable user

---
+++ app/controllers/api/v1/concerns/authenticable_user.rb
@@ -0,0 +1,42 @@
+module Api::V1::Concerns::AuthenticableUser
+  extend ActiveSupport::Concern
+
+  private
+
+  def current_user
+    return unless token && payload && active_refresh_token?
+
+    User.find_by(id: payload['sub'])
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  def authenticate_user!
+    raise UnauthenticatedError unless user_signed_in?
+  end
+
+  def token
+    @token ||= request.headers['Authorization'].to_s.match(/Bearer (.*)/).to_a.last
+  end
+
+  def payload
+    @payload ||= JWT.decode(
+      token,
+      ApplicationConfig.fetch('JWT_SECRET_TOKEN'),
+      true,
+      algorithm: 'HS256'
+    ).first
+  rescue JWT::DecodeError
+    {}
+  end
+
+  def jti
+    payload['jti']
+  end
+
+  def active_refresh_token?
+    RefreshToken.active.exists?(jti: jti)
+  end
+end
