615d06901ad7c62fb07b0ead01e6ba2708b4f599
maxbarsukov <maximbarsukov@bk.ru>
2022-02-13 20:06:36 +0300

    test: :rotating_light: add specs for jobs

---
+++ spec/jobs/register_activity_job_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe RegisterActivityJob do
+  let(:user_id) { 570_242 }
+
+  context 'when user exists' do
+    let!(:user) { create :user, id: user_id }
+
+    it 'calls interactor to create activity' do
+      expect(Activity::Create).to receive(:call!).with(user: user, event: :user_registered)
+
+      described_class.perform_now(user_id, :user_registered)
+    end
+  end
+
+  context 'when user does not exist' do
+    it 'raises' do
+      expect { described_class.perform_now(user_id, :user_registered) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
