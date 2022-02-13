a1e1035d47ccc9cb922333d6c852a8a500056fd7
maxbarsukov <maximbarsukov@bk.ru>
2022-02-13 20:00:52 +0300

    feat: :sparkles: add some interactors for user

---
+++ app/interactors/user/update.rb
@@ -0,0 +1,27 @@
+class User::Update
+  include Interactor
+
+  delegate :user_params, :user, to: :context
+
+  def call
+    context.fail!(error_data: error_data) unless update_user_form.valid? && update_user
+  end
+
+  after do
+    RegisterActivityJob.perform_later(user.id, :user_updated)
+  end
+
+  private
+
+  def update_user
+    user.update(update_user_form.model_attributes)
+  end
+
+  def update_user_form
+    @update_user_form ||= UpdateUserForm.new(user).assign_attributes(user_params)
+  end
+
+  def error_data
+    { message: 'Record Invalid', detail: update_user_form.errors.to_a }
+  end
+end
