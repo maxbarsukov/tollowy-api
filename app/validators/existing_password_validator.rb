98cf22aff38d69e2162efd555e8273a57b5f71bd
maxbarsukov <maximbarsukov@bk.ru>
2022-02-13 19:55:54 +0300

    feat: :sparkles: create validators for auth

---
+++ app/validators/existing_password_validator.rb
@@ -0,0 +1,7 @@
+class ExistingPasswordValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    return if record.model.authenticate(value)
+
+    record.errors.add attribute, 'is incorrect'
+  end
+end
