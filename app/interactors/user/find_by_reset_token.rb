7ab0787ca03b145e802b132d405c25ddf9a5a482
maxbarsukov <maximbarsukov@bk.ru>
2022-02-13 20:01:19 +0300

    feat: :sparkles: add more user's interactors

---
+++ app/interactors/user/find_by_reset_token.rb
@@ -0,0 +1,21 @@
+class User::FindByResetToken
+  include Interactor
+
+  delegate :reset_token, to: :context
+
+  def call
+    context.user = find_user
+
+    context.fail!(error_data: error_data) unless context.user
+  end
+
+  private
+
+  def find_user
+    User.find_by(password_reset_token: reset_token)
+  end
+
+  def error_data
+    { message: 'Invalid credentials', status: 401, code: :unauthorized }
+  end
+end
