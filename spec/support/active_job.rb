323f09adbf97f49127808fe887475d44b7901cff
maxbarsukov <maximbarsukov@bk.ru>
2022-02-13 20:09:12 +0300

    test: :rotating_light: add conf for activejob testing

---
+++ spec/support/active_job.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.include ActiveJob::TestHelper
+
+  config.before do
+    ActiveJob::Base.queue_adapter.enqueued_jobs.clear
+    ActiveJob::Base.queue_adapter.performed_jobs.clear
+  end
+end
