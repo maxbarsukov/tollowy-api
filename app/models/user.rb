33c56f4f05cbd12e992be80770f372d636e3bcb1
maxbarsukov <maximbarsukov@bk.ru>
2022-02-13 19:57:17 +0300

    feat: :sparkles: create activity model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :password_reset_token
 
+  has_many :activities, dependent: :destroy
   has_many :refresh_tokens, dependent: :destroy
 
   rolify
