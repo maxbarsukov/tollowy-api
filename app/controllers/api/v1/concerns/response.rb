a3a023ca9f4f0ef92309fd0f59f5c2b0d593865a
maxbarsukov <maximbarsukov@bk.ru>
2022-02-17 21:13:08 +0300

    feat: :sparkles: add json_error concern for better error serialization

--- app/controllers/api/v1/concerns/response.rb
+++ app/controllers/api/v1/concerns/response.rb
@@ -4,4 +4,15 @@ module Api::V1::Concerns::Response
   def json_response(object, status = :ok)
     render json: object, status: status
   end
+
+  def json_error(error_data, status = :unprocessable_entity)
+    case error_data
+    when Array
+      json_response ErrorSerializer.serialize(error_data), status
+    when Hash
+      json_response ErrorSerializer.serialize([error_data]), status
+    else
+      raise UndefinedErrorDataType
+    end
+  end
 end
