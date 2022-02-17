2288b8b97100a2ed55bab83149ea5d37ac2e2be6
maxbarsukov <maximbarsukov@bk.ru>
2022-02-17 20:55:36 +0300

    feat: :sparkles: add routes for auth

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
       root 'home#index'
       resources :users, only: %i[index show]
 
+      post 'auth/sign_up' => 'auth#sign_up'
+      post 'auth/sign_in' => 'auth#sign_in'
+      delete 'auth/sign_out' => 'auth#sign_out'
+
       get '/health_check', to: ->(_env) { [200, {}, ['ok']] }
     end
   end
