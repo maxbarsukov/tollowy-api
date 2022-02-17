1cdb93828ff533b72cbde0393b407fe434519fde
maxbarsukov <maximbarsukov@bk.ru>
2022-02-17 21:14:29 +0300

    feat: :sparkles: add auth controller

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::AuthController < Api::V1::ApiController
+  using StringToBoolean
+
+  def sign_in
+    result = Auth::SignIn.call(auth_params)
+    if result.success?
+      json_response({
+                      access_token: result.access_token,
+                      refresh_token: result.access_token,
+                      me: UserSerializer.new(result.user)
+                    })
+    else
+      json_error result.error_data
+    end
+  end
+
+  def sign_out
+    Auth::SignOut.call(
+      token: token,
+      user: current_user,
+      everywhere: params[:everywhere]&.to_boolean
+    )
+
+    json_response({ message: 'User signed out successfully' })
+  end
+
+  def sign_up
+    result = Auth::SignUp.call(user_params: auth_params)
+
+    if result.success?
+      json_response({
+                      access_token: result.access_token,
+                      refresh_token: result.access_token,
+                      me: UserSerializer.new(result.user)
+                    })
+    else
+      json_error signup_user.error_data
+    end
+  end
+
+  private
+
+  def auth_params
+    params.permit(:email, :password, :everywhere, :username)
+  end
+end
