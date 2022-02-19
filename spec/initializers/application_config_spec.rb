73d7644e35d8ed7f74ae545cbd56e3a8d460ab70
maxbarsukov <maximbarsukov@bk.ru>
2022-02-19 15:35:35 +0300

    test: :rotating_light: add specs for ApplicationConfig

---
+++ spec/initializers/application_config_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ApplicationConfig do
+  describe '.[]' do
+    context 'when ENV var unset' do
+      it 'returns nil' do
+        expect(described_class['UNSET']).to be nil
+      end
+    end
+
+    context 'when ENV var set' do
+      ENV['VAR'] = 'var'
+
+      it 'returns variable' do
+        expect(described_class['VAR']).to eq 'var'
+      end
+
+      it 'dont logs anything' do
+        allow(Rails.logger).to receive(:debug)
+        expect(Rails.logger).not_to receive(:debug).with(anything)
+      end
+    end
+  end
+end
