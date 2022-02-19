0dff83d0b6c3ded569009c7c5bd8249313dd3384
maxbarsukov <maximbarsukov@bk.ru>
2022-02-19 15:34:00 +0300

    fix: :bug: add user password reset interactor

---
+++ app/interactors/user/reset_password.rb
@@ -0,0 +1,15 @@
+class User::ResetPassword
+  include Interactor::Organizer
+  include TransactionalInteractor
+
+  delegate :user, to: :context
+
+  organize User::FindByResetToken,
+           User::UpdatePassword,
+           Auth::CreateAccessToken,
+           Auth::CreateRefreshToken
+
+  after do
+    RegisterActivityJob.perform_later(user.id, :user_reset_password)
+  end
+end
