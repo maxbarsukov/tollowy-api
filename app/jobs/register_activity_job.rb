f9a24864dcaf48d9b366b9423765d86913f9e2ef
maxbarsukov <maximbarsukov@bk.ru>
2022-03-08 17:21:27 +0300

    refactor: :recycle: replace RegisterActivity job to CreateUserEvent

--- app/jobs/register_activity_job.rb
+++
@@ -1,8 +0,0 @@
-class RegisterActivityJob < ApplicationJob
-  queue_as :events
-
-  def perform(user_id, event)
-    user = User.find(user_id)
-    Activity::Create.call!(user: user, event: event)
-  end
-end
