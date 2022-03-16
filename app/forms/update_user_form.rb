f785a70bd224271c0f5b2b0be1b299594167ce99
maxbarsukov <maximbarsukov@bk.ru>
2022-03-16 18:36:47 +0300

    refactor: :recycle: remove unused validaions from user update form

--- app/forms/update_user_form.rb
+++ app/forms/update_user_form.rb
@@ -4,15 +4,10 @@ class UpdateUserForm < ApplicationForm
 
   attr_accessor(*ATTRIBUTES)
 
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :current_password,
             existing_password: true,
             presence: true,
             unless: -> { password.blank? }
-  validates :password,
-            length: { maximum: ActiveModel::SecurePassword::MAX_PASSWORD_LENGTH_ALLOWED },
-            allow_nil: true,
-            allow_blank: true
 
   def attribute_names
     @attribute_names ||= ATTRIBUTES
