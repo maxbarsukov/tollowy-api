9a6f591a8dd7ccadb2ed2ef14a7c87eca69bc915
maxbarsukov <maximbarsukov@bk.ru>
2022-03-18 22:09:27 +0300

    feat: :sparkles: update logout interactor

--- app/interactors/auth/sign_out.rb
+++ app/interactors/auth/sign_out.rb
@@ -4,8 +4,20 @@ class Auth::SignOut
   delegate :user, :token, :everywhere, to: :context
 
   def call
+    context.fail!(error_data: error_data) unless user
     refresh_tokens = user.refresh_tokens
     refresh_tokens = refresh_tokens.where(token: token) unless everywhere
     refresh_tokens.destroy_all
+    context.message = 'User signed out successfully'
+  end
+
+  private
+
+  def error_data
+    ErrorData.new(
+      status: 401,
+      code: :unauthorized,
+      title: 'Invalid credentials'
+    )
   end
 end
