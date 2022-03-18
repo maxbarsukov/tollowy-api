2c8583da2cbaf10028a0f887c6bc8d5d8e45fc71
maxbarsukov <maximbarsukov@bk.ru>
2022-03-18 21:39:50 +0300

    refactor: :recycle: update error serializer

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -2,13 +2,6 @@ class ErrorSerializer
   def self.call(errors)
     return { errors: [] } if errors.empty?
 
-    json = {}
-    json[:errors] = errors.map do |error|
-      error[:code] ||= :unprocessable_entity
-      error[:status] ||= error[:code] ? Rack::Utils.status_code(code).to_s : '422'
-      error
-    end
-
-    json
+    { errors: errors }
   end
 end
