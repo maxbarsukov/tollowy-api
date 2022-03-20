bd69ae3dd608f4f6ebe3249f371b4c0fbba61d71
maxbarsukov <maximbarsukov@bk.ru>
2022-03-20 16:50:10 +0300

    feat: :sparkles: improve role policy

--- app/policies/role_policy.rb
+++ app/policies/role_policy.rb
@@ -4,13 +4,32 @@ class RolePolicy < ApplicationPolicy
   def initialize(current_user, *args)
     @user = current_user
     @user_to_update, @role = *args
+    @error_message = []
 
     require_user_in_good_standing!
   end
 
   def update?
-    user.at_least_a?(:moderator) &&
-      user.role.value > Role.value_for(role) &&
-      user.id != user_to_update.id
+    @error_message << 'User must be at least a moderator' unless user_is_moderator
+    @error_message << 'You cant update your own role' unless user_not_the_same
+    @error_message << 'You cant assign role higher then yours' unless user_assigns_lower_role
+
+    user_is_moderator &&
+      user_assigns_lower_role &&
+      user_not_the_same
+  end
+
+  private
+
+  def user_is_moderator
+    @user_is_moderator = user.at_least_a?(:moderator)
+  end
+
+  def user_not_the_same
+    @user_not_the_same = user.id != user_to_update.id
+  end
+
+  def user_assigns_lower_role
+    @user_assigns_lower_role = user.role.value > Role.value_for(role)
   end
 end
