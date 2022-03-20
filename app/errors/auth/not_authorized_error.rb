6fb3e21d81b02ef93f66b39f19b93cfe758d618f
maxbarsukov <maximbarsukov@bk.ru>
2022-03-20 16:48:53 +0300

    feat: :sparkles: update NotAuthorizedError

--- app/errors/auth/not_authorized_error.rb
+++ app/errors/auth/not_authorized_error.rb
@@ -4,4 +4,17 @@
 # By inheriting from Pundit::NotAuthorizedError, we can refactor our code to use an application
 # specific error instead of an error from a dependency.
 class Auth::NotAuthorizedError < Pundit::NotAuthorizedError
+  def initialize(options = {})
+    message = options[:message]
+    options = options.except(:message)
+
+    super(options) && return if message.blank?
+
+    case message
+    when String
+      super(message)
+    when Array
+      super(message.empty? ? options : message.to_sentence)
+    end
+  end
 end
