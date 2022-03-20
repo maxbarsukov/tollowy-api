9c99de81ed988aba4564287d61e07d142deb7b84
maxbarsukov <maximbarsukov@bk.ru>
2022-03-20 16:49:13 +0300

    feat: :sparkles: use NotAuthorizedError

--- app/controllers/api/v1/concerns/pundit_authorizer.rb
+++ app/controllers/api/v1/concerns/pundit_authorizer.rb
@@ -7,7 +7,8 @@ module Api::V1::Concerns::PunditAuthorizer
     pundit_policy = policy.new(current_user, *args)
     return if pundit_policy.public_send(query)
 
-    raise Pundit::NotAuthorizedError,
+    raise Auth::NotAuthorizedError,
+          message: pundit_policy.error_message,
           query: query,
           record: args.first,
           policy: pundit_policy
