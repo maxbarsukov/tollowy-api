c9796dbbd1a210e2123bc2c4087b821fc7539c19
maxbarsukov <maximbarsukov@bk.ru>
2022-03-21 21:35:03 +0300

    feat: :sparkles: fix UserRequiredError

--- app/errors/auth/user_required_error.rb
+++ app/errors/auth/user_required_error.rb
@@ -1,4 +1,4 @@
 # Raise this exception when an action requires an authenticated user but the request has no
 # authenticated user.
-class Auth::UserRequiredError < Auth::NotAuthorizedError
+class Auth::UserRequiredError < Pundit::NotAuthorizedError
 end
