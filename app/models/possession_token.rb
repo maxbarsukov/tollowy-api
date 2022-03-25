7fb7630091940ed7dec8ebaf572c8ac4c8746c60
maxbarsukov <maximbarsukov@bk.ru>
2022-03-25 16:34:34 +0300

    docs: :books: generate annotate info to models

--- app/models/possession_token.rb
+++ app/models/possession_token.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: possession_tokens
+#
+#  id         :bigint           not null, primary key
+#  value      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_possession_tokens_on_user_id  (user_id)
+#  index_possession_tokens_on_value    (value) UNIQUE
+#
 class PossessionToken < ApplicationRecord
   belongs_to :user
 
--- app/models/refresh_token.rb
+++ app/models/refresh_token.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: refresh_tokens
+#
+#  id         :bigint           not null, primary key
+#  expires_at :datetime         not null
+#  jti        :string
+#  token      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_refresh_tokens_on_jti      (jti)
+#  index_refresh_tokens_on_token    (token)
+#  index_refresh_tokens_on_user_id  (user_id)
+#
 class RefreshToken < ApplicationRecord
   belongs_to :user
 
