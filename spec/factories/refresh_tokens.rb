52de16c7c0687071fd6a11c5e6f8c0c9cec05a30
maxbarsukov <maximbarsukov@bk.ru>
2022-03-25 16:35:08 +0300

    docs: :books: generate annotate info to spec/factories

--- spec/factories/refresh_tokens.rb
+++ spec/factories/refresh_tokens.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: refresh_tokens
+#
+#  id         :bigint           not null, primary key
+#  expires_at :datetime         not null
+#  jti        :string
+#  token      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_refresh_tokens_on_jti      (jti)
+#  index_refresh_tokens_on_token    (token)
+#  index_refresh_tokens_on_user_id  (user_id)
+#
 FactoryBot.define do
   factory :refresh_token do
     association :user
