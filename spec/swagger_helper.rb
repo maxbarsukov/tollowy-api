c9e3b442a99764ad98e0b01de71798d02a5bdbf8
maxbarsukov <maximbarsukov@bk.ru>
2022-03-26 16:23:24 +0300

    chore: :wrench: update rswag components

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -2,8 +2,8 @@
 
 require 'rails_helper'
 
-# rubocop:disable Naming/VariableNumber
-RSpec.configure do |config|
+# rubocop:disable Naming/VariableNumber Metrics/BlockLength
+RSpec.configure do |config| # rubocop:disable Metrics/BlockLength
   # Specify a root folder where Swagger JSON files are generated
   # NOTE: If you're using the rswag-api to serve API descriptions, you'll need
   # to ensure that it's configured to serve Swagger from the same folder
@@ -78,6 +78,8 @@ RSpec.configure do |config|
             required: %w[email username created_at role]
           },
           unauthorized_error: {
+            title: 'Unauthorized Error',
+            description: 'User must sign in before continuing',
             type: :object,
             properties: {
               errors: {
@@ -95,7 +97,29 @@ RSpec.configure do |config|
             },
             required: ['errors']
           },
-          user_not_found_response: {
+          invalid_credentials_error: {
+            title: 'Invalid Credentials Error',
+            description: "User's token is invalid",
+            type: :object,
+            properties: {
+              errors: {
+                type: :array,
+                items: {
+                  type: :object,
+                  properties: {
+                    status: { type: :string, enum: ['401'] },
+                    code: { type: :string, enum: ['unauthorized'] },
+                    title: { type: :string, enum: ['Invalid credentials'] }
+                  },
+                  required: %w[status code title]
+                }
+              }
+            },
+            required: ['errors']
+          },
+          user_not_found_error: {
+            title: 'User Not Found',
+            description: 'User ID is invalid',
             type: :object,
             properties: {
               errors: {
@@ -122,6 +146,37 @@ RSpec.configure do |config|
             required: ['message']
           },
           response_root_get_401: { '$ref' => '#/components/schemas/unauthorized_error' },
+          # /api/v1/auth/sign_in
+          response_auth_sign_in: {
+            type: :object,
+            properties: {
+              data: {
+                type: :object,
+                properties: {
+                  access_token: { type: :string },
+                  refresh_token: { type: :string },
+                  me: { '$ref' => '#/components/schemas/user' }
+                },
+                required: %w[access_token refresh_token me]
+              }
+            },
+            required: ['data']
+          },
+          # /api/v1/auth/sign_out
+          response_auth_sign_out: {
+            type: :object,
+            properties: {
+              data: {
+                type: :object,
+                properties: {
+                  message: { type: :string }
+                },
+                required: ['message']
+              }
+            },
+            required: ['data']
+          },
+          response_auth_sign_out_401: { '$ref' => '#/components/schemas/invalid_credentials_error' },
           # /api/v1/users
           response_users_get: {
             type: :object,
@@ -131,13 +186,40 @@ RSpec.configure do |config|
             required: ['data']
           },
           # /api/v1/users/{id}
-          response_users_id_get_404: { '$ref' => '#/components/schemas/user_not_found_response' },
+          response_users_id_get_404: { '$ref' => '#/components/schemas/user_not_found_error' },
           response_users_id_get: {
             type: :object,
             properties: {
               data: { '$ref' => '#/components/schemas/user' }
             },
             required: ['data']
+          },
+          # Parameters
+          # /api/v1/auth/sign_in
+          parameter_auth_sign_in: {
+            type: :object,
+            properties: {
+              data: {
+                type: :object,
+                properties: {
+                  type: { type: :string, enum: ['auth'] },
+                  attributes: {
+                    type: :object,
+                    properties: {
+                      email: { type: :string },
+                      password: { type: :string }
+                    },
+                    required: %w[email password]
+                  }
+                },
+                required: %w[type attributes]
+              }
+            }
+          },
+          # /api/v1/auth/sign_out
+          parameter_auth_sign_out: {
+            type: :string, nullable: true,
+            enum: %w[true false]
           }
         }
       }
