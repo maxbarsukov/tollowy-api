cbd52efee54d56e59cb51760c51792a702ae2463
maxbarsukov <maximbarsukov@bk.ru>
2022-04-10 19:02:47 +0300

    test: :rotating_light: add spec for admin/sessions

---
+++ spec/controllers/admin/sessions_controller_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe Admin::SessionsController, type: :controller do
+  include_context 'with active admin routes'
+
+  describe '#destroy' do
+    it 'resets all sessions' do
+      session[:a] = '1'
+      session[:b] = '2'
+      expect(session[:a]).to eq('1')
+      delete :destroy
+      expect(session[:a]).to be nil
+      expect(session[:b]).to be nil
+    end
+
+    it 'redirects to admin_root' do
+      delete :destroy
+      expect(response).to redirect_to(admin_root_path)
+    end
+  end
+
+  describe '#new' do
+    let(:user) { create(:user, :with_admin_role) }
+
+    it 'redirects if user signed in' do
+      request.headers['Authorization'] = ApiHelper.authenticated_header(user: user)
+      get :new
+      expect(response).to redirect_to(admin_root_path)
+    end
+
+    it 'renders login page if not signed in' do
+      expect(get(:new)).to render_template(:new)
+    end
+  end
+
+  describe '#create' do
+    let(:user) { create(:user, :with_known_data) }
+
+    context 'with valid data' do
+      subject(:req) { post :create, params: { session: { email: user.email, password: user.password } } }
+
+      it 'authenticates and redirects' do
+        req
+        expect(session[:access_token]).not_to be nil
+      end
+
+      it 'redirects to admin page' do
+        req
+        expect(response).to redirect_to(admin_root_path)
+      end
+    end
+
+    context 'with invalid data' do
+      subject(:req) { post :create, params: { session: { email: "#{user.email}111", password: user.password } } }
+
+      it 'not authenticates' do
+        req
+        expect(session[:access_token]).to be nil
+      end
+
+      it 'redirects to login page' do
+        expect(req).to render_template(:new)
+      end
+    end
+  end
+end
