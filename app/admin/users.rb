41604f6d95e8903fa0bd61cc2f9f144dd073c78e
maxbarsukov <maximbarsukov@bk.ru>
2022-04-10 17:24:01 +0300

    feat: :sparkles: add username to admin/user

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -27,8 +27,8 @@ ActiveAdmin.register User do
   form do |f|
     f.inputs do
       f.input :email
+      f.input :username
       f.input :password
-      f.input :role
     end
     f.actions
   end
