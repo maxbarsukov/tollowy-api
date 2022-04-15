74c0c91d268cd790b5740da833cd90894edad550
maxbarsukov <maximbarsukov@bk.ru>
2022-04-15 22:52:39 +0300

    feat: :sparkles: add recent posts to admin page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -13,16 +13,21 @@ ActiveAdmin.register_page 'Dashboard' do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel 'Recent Posts' do
+          ul do
+            Post.includes([:user]).order(created_at: :desc).take(5).map do |post|
+              li do
+                span link_to(truncate(post.body, length: 15), admin_post_path(post))
+                span 'by'
+                span link_to(post.user.username, admin_user_path(post))
+              end
+            end
+          end
+        end
+      end
+    end
 
     #   column do
     #     panel "Info" do
