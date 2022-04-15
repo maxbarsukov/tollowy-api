f6b8a15ba1812a0edd746a689188f40d47632291
maxbarsukov <maximbarsukov@bk.ru>
2022-04-15 22:11:23 +0300

    test: :rotating_light: create posts factory

---
+++ spec/factories/posts.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  body       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+FactoryBot.define do
+  factory :post do
+    body { Faker::Lorem.paragraph(sentence_count: 2) }
+
+    association :user, factory: :user do
+      with_user_role
+    end
+  end
+end
