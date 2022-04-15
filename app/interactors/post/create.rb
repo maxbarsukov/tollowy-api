9e17e9365b8d58a7f0c4d29e4f3d0b58e9b9d06f
maxbarsukov <maximbarsukov@bk.ru>
2022-04-15 22:07:10 +0300

    feat: :sparkles: add post::create interactor

---
+++ app/interactors/post/create.rb
@@ -0,0 +1,26 @@
+class Post::Create
+  include Interactor
+
+  delegate :post_params, :current_user, to: :context
+
+  def call
+    context.post = current_user.posts.new(post_params)
+
+    fail! unless context.post.save
+  end
+
+  private
+
+  def fail!
+    context.fail!(error_data: error_data)
+  end
+
+  def error_data
+    ErrorData.new(
+      status: 422,
+      code: :unprocessable_entity,
+      title: 'Record Invalid',
+      detail: context.post.errors.to_a
+    )
+  end
+end
