0df3fbad9b444bedd6370791fe3e95f9a44f0e25
maxbarsukov <maximbarsukov@bk.ru>
2022-04-17 13:03:41 +0300

    refactor: :recycle: monkey patch pundit authorize to use own error type

---
+++ config/initializers/monkey_patches/pundit.rb
@@ -0,0 +1,18 @@
+Pundit.module_eval do
+  class << self
+    def authorize(user, possibly_namespaced_record, query, policy_class: nil, cache: {})
+      record = pundit_model(possibly_namespaced_record)
+      policy = if policy_class then policy_class.new(user, record)
+               else cache[possibly_namespaced_record] ||= policy!(user, possibly_namespaced_record)
+               end
+
+      unless policy.public_send(query)
+        raise Auth::BasicAuthError,
+              error_code: policy.error_code,
+              query: query, record: record, policy: policy
+      end
+
+      record
+    end
+  end
+end
