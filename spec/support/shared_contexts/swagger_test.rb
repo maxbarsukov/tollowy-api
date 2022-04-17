c40b9f61b8d70dbb5fce3e780d66bbad12b4d373
maxbarsukov <maximbarsukov@bk.ru>
2022-04-17 21:12:24 +0300

    fix: :bug: fix rswag bad base url handling

--- spec/support/shared_contexts/swagger_test.rb
+++ spec/support/shared_contexts/swagger_test.rb
@@ -2,7 +2,14 @@
 
 require 'rails_helper'
 
+# rubocop:disable Style/IfUnlessModifier
 shared_context 'with swagger test' do
+  before do |example|
+    unless example.metadata[:path_item][:template].starts_with? '/api/v1'
+      example.metadata[:path_item][:template] = example.metadata[:path_item][:template].prepend('/api/v1')
+    end
+  end
+
   run_test!
 
   after do |example|
@@ -11,5 +18,8 @@ shared_context 'with swagger test' do
         example: JSON.parse(response.body, symbolize_names: true)
       }
     }
+
+    example.metadata[:path_item][:template].delete_prefix!('/api/v1')
   end
 end
+# rubocop:enable Style/IfUnlessModifier
