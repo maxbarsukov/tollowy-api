ca423d0b0199e97dd017439912eccd3fefb2f24d
maxbarsukov <maximbarsukov@bk.ru>
2022-04-17 21:18:19 +0300

    style: :art: remove unused rubocop comments

--- spec/support/shared_contexts/swagger_test.rb
+++ spec/support/shared_contexts/swagger_test.rb
@@ -2,7 +2,6 @@
 
 require 'rails_helper'
 
-# rubocop:disable Style/IfUnlessModifier
 shared_context 'with swagger test' do
   before do |example|
     unless example.metadata[:path_item][:template].starts_with? '/api/v1'
@@ -22,4 +21,3 @@ shared_context 'with swagger test' do
     example.metadata[:path_item][:template].delete_prefix!('/api/v1')
   end
 end
-# rubocop:enable Style/IfUnlessModifier
