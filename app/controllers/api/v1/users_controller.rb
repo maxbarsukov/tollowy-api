aeadf19ae83f467ca1b1fbe2f468472932938b33
maxbarsukov <maximbarsukov@bk.ru>
2022-04-18 21:12:08 +0300

    style: :art: apply rubocop fixes for app/

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,41 +1,39 @@
-module Api::V1
-  class UsersController < Api::V1::ApiController
-    before_action :set_user, only: %i[show update]
+class Api::V1::UsersController < Api::V1::ApiController
+  before_action :set_user, only: %i[show update]
 
-    # GET /api/v1/users
-    def index
-      @users = User.includes(%i[roles roles_users])
-      json_response UserSerializer.call(@users)
+  # GET /api/v1/users
+  def index
+    @users = User.includes(%i[roles roles_users])
+    json_response UserSerializer.call(@users)
+  end
+
+  # GET /api/v1/users/:id
+  def show
+    json_response UserSerializer.call(@user)
+  end
+
+  # PATCH /api/v1/users/:id
+  # PUT /api/v1/users/:id
+  def update
+    authorize_with_multiple(@user, user_params[:role], :update?, RolePolicy) if user_params.key?(:role)
+    authorize @user
+
+    result = User::Update.call(user: @user, user_params: user_params)
+
+    if result.success?
+      json_response UserSerializer.call(result.user)
+    else
+      json_error result.error_data
     end
+  end
 
-    # GET /api/v1/users/:id
-    def show
-      json_response UserSerializer.call(@user)
-    end
+  private
 
-    # PATCH /api/v1/users/:id
-    # PUT /api/v1/users/:id
-    def update
-      authorize_with_multiple(@user, user_params[:role], :update?, RolePolicy) if user_params.key?(:role)
-      authorize @user
+  def set_user
+    @user = User.find(params.require(:id))
+  end
 
-      result = User::Update.call(user: @user, user_params: user_params)
-
-      if result.success?
-        json_response UserSerializer.call(result.user)
-      else
-        json_error result.error_data
-      end
-    end
-
-    private
-
-    def set_user
-      @user = User.find(params.require(:id))
-    end
-
-    def user_params
-      json_params(%i[username email password current_password role])
-    end
+  def user_params
+    json_params(%i[username email password current_password role])
   end
 end
