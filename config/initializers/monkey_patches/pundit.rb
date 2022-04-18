3a898e209bd7c22e5a513d685814e3d20fad02a8
maxbarsukov <maximbarsukov@bk.ru>
2022-04-18 21:12:25 +0300

    style: :art: apply rubocop fixes for config/

--- config/initializers/monkey_patches/pundit.rb
+++ config/initializers/monkey_patches/pundit.rb
@@ -3,13 +3,13 @@ Pundit.module_eval do
     def authorize(user, possibly_namespaced_record, query, policy_class: nil, cache: {})
       record = pundit_model(possibly_namespaced_record)
       policy = if policy_class then policy_class.new(user, record)
-               else cache[possibly_namespaced_record] ||= policy!(user, possibly_namespaced_record)
+               else
+                 cache[possibly_namespaced_record] ||= policy!(user, possibly_namespaced_record)
                end
 
       unless policy.public_send(query)
-        raise Auth::BasicAuthError,
-              error_code: policy.error_code,
-              query: query, record: record, policy: policy
+        raise Auth::BasicAuthError, error_code: policy.error_code,
+                                    query: query, record: record, policy: policy
       end
 
       record
