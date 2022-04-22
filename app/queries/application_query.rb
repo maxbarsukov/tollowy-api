029c76d9b2fdef424a4b3b518e8cc0fcc74d3996
maxbarsukov <maximbarsukov@bk.ru>
2022-04-22 22:38:55 +0300

    linter: :nail_care: disable rubocop for gem-overrided code

--- app/queries/application_query.rb
+++ app/queries/application_query.rb
@@ -3,10 +3,12 @@ class ApplicationQuery < Jsonapi::QueryBuilder::BaseQuery
     super(collection, params.to_unsafe_hash)
   end
 
+  # rubocop:disable Performance/MethodObjectAsBlock
   def results
     collection
       .yield_self(&method(:add_includes))
       .yield_self(&method(:sort))
       .yield_self(&method(:filter))
   end
+  # rubocop:enable Performance/MethodObjectAsBlock
 end
