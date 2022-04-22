59073a881600269d0d1fefa467f4347c00cb9b61
maxbarsukov <maximbarsukov@bk.ru>
2022-04-22 21:58:25 +0300

    fix: :bug: disable pagination in jsonapi_query_builder

--- app/queries/application_query.rb
+++ app/queries/application_query.rb
@@ -2,4 +2,11 @@ class ApplicationQuery < Jsonapi::QueryBuilder::BaseQuery
   def initialize(collection, params)
     super(collection, params.to_unsafe_hash)
   end
+
+  def results
+    collection
+      .yield_self(&method(:add_includes))
+      .yield_self(&method(:sort))
+      .yield_self(&method(:filter))
+  end
 end
