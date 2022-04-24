25699233d9d2c71d9f0b3c6b4be7a849d0682780
maxbarsukov <maximbarsukov@bk.ru>
2022-04-24 14:18:43 +0300

    linter: :nail_care: disable Metrics/BlockLength for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
   mount Rswag::Ui::Engine => '/docs'
   mount Rswag::Api::Engine => '/docs'
 
