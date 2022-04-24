006ec66de2960d3eca4a1e7640f4e7b89dc35b53
maxbarsukov <maximbarsukov@bk.ru>
2022-04-24 15:12:32 +0300

    test: :rotating_light: add spec for Admin::Comments policy

---
+++ spec/policies/active_admin/comment_policy_spec.rb
@@ -0,0 +1,39 @@
+describe ActiveAdmin::CommentPolicy, type: :policy do
+  subject(:comment_policy) { described_class }
+
+  let(:user) { create(:user, :with_admin_role) }
+
+  context 'when current user is author' do
+    permissions :destroy? do
+      it 'grants access to destroy comment' do
+        comment = ActiveAdmin::Comment.create(
+          resource_id: user.id,
+          namespace: 'admin',
+          body: 'My comment body',
+          resource_type: 'User',
+          author_id: user.id,
+          author_type: 'User'
+        )
+        expect(comment_policy).to permit(user, comment)
+      end
+    end
+  end
+
+  context 'when current user is not author' do
+    permissions :destroy? do
+      it 'denies access to destroy comment' do
+        another_user = create(:user, :with_admin_role)
+
+        comment = ActiveAdmin::Comment.create(
+          resource_id: another_user.id,
+          namespace: 'admin',
+          body: 'My comment body',
+          resource_type: 'User',
+          author_id: another_user.id,
+          author_type: 'User'
+        )
+        expect(comment_policy).not_to permit(user, comment)
+      end
+    end
+  end
+end
