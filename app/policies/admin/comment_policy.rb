38be79f5e29ebe2bd75a59afff317e532cd2f6e5
maxbarsukov <maximbarsukov@bk.ru>
2022-05-15 19:01:16 +0300

    feat: :sparkles: add policies for comments

---
+++ app/policies/admin/comment_policy.rb
@@ -0,0 +1,15 @@
+class Admin::CommentPolicy < Admin::ApplicationPolicy
+  def update?
+    manage? && super
+  end
+
+  def destroy?
+    manage? && super
+  end
+
+  private
+
+  def manage?
+    record.user.role.value < user.role.value || record.user_id == user.id
+  end
+end
