e3c8fd9e554c96b7e26242fdaf49e77a812a7194
maxbarsukov <maximbarsukov@bk.ru>
2022-05-15 18:49:50 +0300

    feat: :sparkles: create commentable concern

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,20 +2,25 @@
 #
 # Table name: posts
 #
-#  id         :bigint           not null, primary key
-#  body       :text             not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :bigint           not null
+#  id             :bigint           not null, primary key
+#  body           :text             not null
+#  comments_count :integer          default(0), not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  user_id        :bigint           not null
 #
 # Indexes
 #
 #  index_posts_on_user_id  (user_id)
 #
 class Post < ApplicationRecord
+  include Commentable
+
   resourcify
 
   belongs_to :user
+
+  counter_culture :user
 
   validates :body, presence: true, length: { in: 1..10_000 }
 end
