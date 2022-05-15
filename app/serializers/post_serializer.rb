8a1f194d62adc4ae521d8222deb5d89eec7b8945
maxbarsukov <maximbarsukov@bk.ru>
2022-05-15 19:02:56 +0300

    feat: :sparkles: improve serializers with posts and comments count

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,8 @@
 class PostSerializer < ApplicationSerializer
-  attributes :body, :created_at, :updated_at
+  attributes :body,
+             :created_at,
+             :updated_at,
+             :comments_count
 
   belongs_to :user
 end
