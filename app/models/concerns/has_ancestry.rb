b680d23f6c982952617e8f3ef26b4ceb789ad2a2
maxbarsukov <maximbarsukov@bk.ru>
2022-05-17 22:05:48 +0300

    perf: :chart_with_upwards_trend: add optimizee method for fetching comments with parent

--- app/models/concerns/has_ancestry.rb
+++ app/models/concerns/has_ancestry.rb
@@ -17,5 +17,11 @@ module HasAncestry
     def parent_exists?
       parent_id && self.class.exists?(id: parent_id)
     end
+
+    def self.with_parents
+      table_name = name.constantize.table_name
+      joins("INNER JOIN #{table_name} AS parents on
+             SUBSTR(#{table_name}.ancestry, STRPOS(#{table_name}.ancestry, '/') + 1)::bigint = parents.id")
+    end
   end
 end
