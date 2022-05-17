b6ad8b3c30cf2d72f4f5da03c93147f32c374768
maxbarsukov <maximbarsukov@bk.ru>
2022-05-17 22:55:35 +0300

    style: :art: fix hash style at comments payload

--- app/payloads/comment/show_payload.rb
+++ app/payloads/comment/show_payload.rb
@@ -3,10 +3,7 @@ class Comment::ShowPayload < ApplicationPayload
     {
       data: (
         comment.subtree.arrange_serializable do |parent, children|
-          {
-            **CommentSerializer.call(parent)[:data],
-            children: children
-          }
+          { **CommentSerializer.call(parent)[:data], children: children }
         end
       ),
       meta: { descendant_count: comment.descendant_ids.count }
