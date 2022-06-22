ce3d7379a84784ce0249210d8fc6cc517df8efef
maxbarsukov <maximbarsukov@bk.ru>
2022-06-22 09:44:09 +0300

    chore: :wrench: update schema info in specs

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :bigint           not null, primary key
+#  ancestry         :string
+#  ancestry_depth   :integer          default(0)
+#  body             :text
+#  children_count   :integer          default(0)
+#  commentable_type :string           not null
+#  deleted          :boolean          default(FALSE)
+#  edited           :boolean          default(FALSE)
+#  edited_at        :datetime
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  commentable_id   :bigint           not null
+#  user_id          :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_ancestry     (ancestry)
+#  index_comments_on_commentable  (commentable_type,commentable_id)
+#  index_comments_on_user_id      (user_id)
+#
 FactoryBot.define do
   factory :comment do
     body { Faker::Lorem.paragraph(sentence_count: 2) }
