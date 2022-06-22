d79acfa639201444b9c015fd943e2d3b45842251
maxbarsukov <maximbarsukov@bk.ru>
2022-06-22 09:40:56 +0300

    feat: :sparkles: create avatar uploader

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,16 @@
+class AvatarUploader < ApplicationUploader
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  def size_range
+    1..(4.megabytes)
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) || model.instance_variable_set(var, SecureRandom.uuid)
+  end
+end
