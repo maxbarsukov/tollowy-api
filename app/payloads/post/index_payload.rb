7bfb7a0b7e8363c9a2e2f21389e28e486c3314a9
maxbarsukov <maximbarsukov@bk.ru>
2022-06-23 15:25:28 +0300

    refactor: :recycle: refactor post payloads

--- app/payloads/post/index_payload.rb
+++ app/payloads/post/index_payload.rb
@@ -1,9 +1,9 @@
 class Post::IndexPayload < Post::Payload
   def self.create(obj)
     {
-      **PostSerializer.call(obj.collection),
-      links: obj.links,
-      meta: obj.meta
+      **PostSerializer.call(obj.posts.collection, { params: obj.options }),
+      links: obj.posts.links,
+      meta: obj.posts.meta
     }
   end
 end
