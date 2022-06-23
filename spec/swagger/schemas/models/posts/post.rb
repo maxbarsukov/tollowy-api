77bc142c7f14f41dd34c9335bdd34c100d8433d0
maxbarsukov <maximbarsukov@bk.ru>
2022-06-23 15:22:37 +0300

    test: :rotating_light: add my_rate to posts swagger schema

--- spec/swagger/schemas/models/posts/post.rb
+++ spec/swagger/schemas/models/posts/post.rb
@@ -8,7 +8,13 @@ class Schemas::Post < Schemas::Base
         id: { type: :string },
         type: { type: :string },
         attributes: Schemas::PostAttributes.ref,
-        relationships: Schemas::PostRelationships.ref
+        relationships: Schemas::PostRelationships.ref,
+        meta: {
+          type: :object,
+          properties: {
+            my_rate: { type: :integer, nullable: true }
+          }
+        }
       },
       required: %w[id type]
     }
