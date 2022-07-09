a3d31bfa9bea7f7562868d6bd95e1c4a35417fa7
maxbarsukov <maximbarsukov@bk.ru>
2022-07-09 14:57:21 +0300

    refactor: :recycle: refactor posts#index interactor

--- app/interactors/post/index.rb
+++ app/interactors/post/index.rb
@@ -2,32 +2,44 @@ class Post::Index
   include Interactor
 
   delegate :current_user, :controller, :posts, to: :context
+  attr_accessor :query_params, :pagination_params
 
   # rubocop:disable Metrics/AbcSize
   def call
-    query_params = controller.send(:query_params)
-    pagination_params = controller.send(:pagination_params)
+    set_params!
 
     filtered_posts = PostsFilter.new.call(posts, controller.params)
     post_query = PostQuery.new(filtered_posts, query_params)
 
     paginated = controller.send(:paginate, post_query.results, pagination_params)
 
-    if signed_in?
-      paginated.collection = LikedVotableQuery.new(
-        paginated.collection, Post, current_user
-      ).call
-    end
+    paginated.collection = load_votable_data(paginated.collection)
+    paginated.collection = paginated.collection.includes([:tags])
 
     context.posts = paginated
     set_options!
   end
   # rubocop:enable Metrics/AbcSize
 
+  private
+
+  def load_votable_data(collection)
+    return collection unless signed_in?
+
+    LikedVotableQuery.new(
+      collection, Post, current_user
+    ).call
+  end
+
   def signed_in?
     return @signed_in if defined? @signed_in
 
     @signed_in = controller.send(:user_signed_in?)
+  end
+
+  def set_params!
+    @query_params = controller.send(:query_params)
+    @pagination_params = controller.send(:pagination_params)
   end
 
   def set_options!
