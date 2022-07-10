3830e71b2ea77548954e55953f172526af2f31c3
maxbarsukov <maximbarsukov@bk.ru>
2022-07-10 11:03:20 +0300

    feat: :sparkles: implement interactor for posts#tags

---
+++ app/interactors/post/tags.rb
@@ -0,0 +1,16 @@
+class Post::Tags
+  include Interactor
+
+  delegate :controller, :post, :current_user, to: :context
+
+  def call
+    result = Tag::Index.call(
+      controller: controller,
+      tags: post.tags,
+      current_user: current_user
+    )
+
+    context.tags = result.tags
+    context.options = result.options
+  end
+end
---
+++ app/payloads/post/tags_payload.rb
@@ -0,0 +1,1 @@
+Post::TagsPayload = Tag::IndexPayload
