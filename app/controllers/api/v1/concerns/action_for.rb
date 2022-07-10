9a1be0cf76ad08f117c3322530069ccfbfb3203b
maxbarsukov <maximbarsukov@bk.ru>
2022-07-10 12:42:42 +0300

    style: :art: fix some rubocop offenses

--- app/controllers/api/v1/concerns/action_for.rb
+++ app/controllers/api/v1/concerns/action_for.rb
@@ -7,7 +7,7 @@ module Api::V1::Concerns::ActionFor
     module_name = controller_name.classify
     name = name.to_s.camelize
     # if controller is PostsController, ctx is { post: @post }
-    ctx ||= { :"#{module_name.downcase}" => instance_variable_get(("@#{module_name.downcase}").intern) }
+    ctx ||= { "#{module_name.downcase}": instance_variable_get("@#{module_name.downcase}".intern) }
 
     result = "#{module_name}::#{name}".constantize.call(interactor_context(ctx))
     payload result, "#{module_name}::#{name}Payload".constantize, status: status || :ok
