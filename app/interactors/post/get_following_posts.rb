92701caab859aa0ab2dc99f8c5d381ff936f7ddc
maxbarsukov <maximbarsukov@bk.ru>
2022-07-11 17:19:53 +0300

    fix: :bug: fix following posts fetching

--- app/interactors/post/get_following_posts.rb
+++ app/interactors/post/get_following_posts.rb
@@ -4,7 +4,7 @@ class Post::GetFollowingPosts
   delegate :current_user, to: :context
 
   def call
-    posts = Post.where(id: tags_posts | users_posts)
+    posts = Post.where(id: tags_posts_ids | users_posts_ids)
 
     context.following_posts_exists = posts.present?
     context.posts = posts
@@ -13,14 +13,14 @@ class Post::GetFollowingPosts
   private
 
   def tags_ids
-    current_user.following_by_type(Tag::NAME, { model: Tag })
+    current_user.following_by_type(Tag::NAME, { model: Tag }).ids
   end
 
-  def tags_posts
+  def tags_posts_ids
     ActsAsTaggableOn::Tagging.where(tag_id: tags_ids).distinct.pluck(:taggable_id)
   end
 
-  def users_posts
-    Post.where(user_id: current_user.following_by_type('User').pluck(:id))
+  def users_posts_ids
+    Post.where(user_id: current_user.following_by_type('User').pluck(:id)).ids
   end
 end
