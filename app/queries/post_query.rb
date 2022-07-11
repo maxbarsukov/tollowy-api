d33987096054e4f61bbaafee029c109e3d530b18
maxbarsukov <maximbarsukov@bk.ru>
2022-07-11 12:42:24 +0300

    feat: :sparkles: add score and likes_count param for posts sorting

--- app/queries/post_query.rb
+++ app/queries/post_query.rb
@@ -2,5 +2,8 @@ class PostQuery < ApplicationQuery
   default_sort created_at: :desc
 
   sorts_by :body
+  sorts_by :score
+  sorts_by :likes_count
+  sorts_by :dislikes_count
   sorts_by :created_at
 end
