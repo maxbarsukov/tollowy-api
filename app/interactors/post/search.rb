108166e3b6454496cfdb038cde0cde2d58906281
maxbarsukov <maximbarsukov@bk.ru>
2022-07-15 11:37:42 +0300

    fix: :bug: fix elsatic error on negative post score

--- app/interactors/post/search.rb
+++ app/interactors/post/search.rb
@@ -11,7 +11,7 @@ class Post::Search
       controller.params[:q],
       fields: %w[body^3 tag_list],
       match: :text_middle,
-      boost_by: { cached_votes_score: { factor: 1.5 } },
+      boost_by: { cached_votes_total: { factor: 1.5 } },
       boost_by_recency: { created_at: { factor: 1.6, scale: '7d', decay: 0.4 } },
       misspellings: { below: 5, edit_distance: 2 },
       includes:
