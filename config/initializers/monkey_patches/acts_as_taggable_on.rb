bcb5b1392ec2ed4089fa13b439c13ca60b0be443
maxbarsukov <maximbarsukov@bk.ru>
2022-07-15 12:36:15 +0300

    fix: :bug: reindex tag after post update

--- config/initializers/monkey_patches/acts_as_taggable_on.rb
+++ config/initializers/monkey_patches/acts_as_taggable_on.rb
@@ -1,3 +1,9 @@
 class ActsAsTaggableOn::Tagging
   belongs_to :tagger, polymorphic: true, optional: true
+
+  after_save :reindex_taggable
+
+  def reindex_taggable
+    taggable.reload.reindex
+  end
 end
