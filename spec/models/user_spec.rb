cbca6c5604d808cbd433c4881dcf577bfdda9d03
maxbarsukov <maximbarsukov@bk.ru>
2022-07-17 17:27:42 +0300

    chore: :wrench: update annotations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,8 @@
 #
 #  id                     :bigint           not null, primary key
 #  avatar                 :string
+#  bio                    :text
+#  blog                   :string
 #  comments_count         :integer          default(0), not null
 #  confirmed_at           :datetime
 #  current_sign_in_at     :datetime
@@ -18,10 +20,13 @@
 #  last_followed_at       :datetime
 #  last_sign_in_at        :datetime
 #  last_sign_in_ip        :string
+#  location               :string
 #  password_digest        :string           not null
 #  password_reset_sent_at :datetime
 #  password_reset_token   :string
 #  posts_count            :integer          default(0), not null
+#  provider               :string           default("email"), not null
+#  provider_uid           :string
 #  sign_in_count          :integer          default(0), not null
 #  username               :string           not null
 #  created_at             :datetime         not null
@@ -29,9 +34,10 @@
 #
 # Indexes
 #
-#  index_users_on_email                 (email) UNIQUE
-#  index_users_on_password_reset_token  (password_reset_token) UNIQUE
-#  index_users_on_username              (username) UNIQUE
+#  index_users_on_email                      (email) UNIQUE
+#  index_users_on_password_reset_token       (password_reset_token) UNIQUE
+#  index_users_on_provider_and_provider_uid  (provider,provider_uid) UNIQUE
+#  index_users_on_username                   (username) UNIQUE
 #
 require 'rails_helper'
 
