c5074bdef7c283c4dc4bb4356601cdb3bbdb7ee3
maxbarsukov <maximbarsukov@bk.ru>
2022-07-21 18:10:57 +0300

    fix: :bug: dont set vk auth context if existing user passed

--- app/interactors/auth/vk/set_context.rb
+++ app/interactors/auth/vk/set_context.rb
@@ -4,6 +4,8 @@ class Auth::Vk::SetContext
   delegate :vk_response, to: :context
 
   def call
+    return if context.existing_user
+
     context.vk_access_token = vk_response[:access_token]
     context.user_id = vk_response[:user_id]
     context.new_email_passed = new_email_passed
