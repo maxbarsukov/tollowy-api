b49bccb2b095d0fe5f14cc0fccb60c3ff5e6c2a3
maxbarsukov <maximbarsukov@bk.ru>
2022-07-21 14:43:23 +0300

    feat: :sparkles: implement vk adapter

---
+++ app/adapters/vk_adapter.rb
@@ -0,0 +1,28 @@
+class VkAdapter
+  include HttpAdapter
+
+  attr_reader :connection
+
+  def initialize(access_token, v = '5.131')
+    @connection = Faraday.new(
+      url: 'https://api.vk.com',
+      params: { access_token:, v: },
+      request: { timeout: 3 }
+    )
+  end
+
+  # Override #check_success method because VK always returns 200
+  def check_success(response) = Oj.load(response.body)['error'].nil?
+
+  # @return [Response::Vk::UserGetResponse, Response::Vk::Error]
+  def user_get(user_id, fields = 'about,nickname,screen_name,status,city,country,site')
+    make_request(
+      @connection.get('/method/users.get') do |req|
+        req.params['user_ids'] = user_id
+        req.params['fields'] = fields
+      end,
+      Response::Vk::UserGetResponse,
+      Response::Vk::Error
+    )
+  end
+end
