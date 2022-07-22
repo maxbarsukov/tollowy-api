4e0cbaa167f62bef9e499fb2addf7f687374486e
maxbarsukov <maximbarsukov@bk.ru>
2022-07-22 18:36:40 +0300

    feat: :sparkles: add method to fetch access_token to VkAdapter

--- app/adapters/vk_adapter.rb
+++ app/adapters/vk_adapter.rb
@@ -1,9 +1,10 @@
 class VkAdapter
   include HttpAdapter
 
-  attr_reader :connection
+  attr_reader :connection, :api_version
 
-  def initialize(access_token, version = '5.131')
+  def initialize(access_token = nil, version = '5.131')
+    @api_version = version
     @connection = Faraday.new(
       url: 'https://api.vk.com',
       params: { access_token:, v: version },
@@ -12,7 +13,22 @@ class VkAdapter
   end
 
   # Override #check_success method because VK always returns 200
-  def check_success(response) = Oj.load(response.body)['error'].nil?
+  def check_success(response)
+    json = Oj.load(response.body)
+    json['error'].nil? && json['error_description'].nil?
+  end
+
+  # @return [Response::Vk::AccessTokenResponse, Response::Vk::AccessTokenError]
+  def access_token(code, redirect_uri)
+    make_request(
+      create_oauth_connection.post('/access_token') do |req|
+        req.params['code'] = code
+        req.params['redirect_uri'] = redirect_uri
+      end,
+      Response::Vk::AccessTokenResponse,
+      Response::Vk::AccessTokenError
+    )
+  end
 
   # @return [Response::Vk::UserGetResponse, Response::Vk::Error]
   def user_get(fields = 'about,nickname,screen_name,status,city,country,site')
@@ -22,6 +38,20 @@ class VkAdapter
       end,
       Response::Vk::UserGetResponse,
       Response::Vk::Error
+    )
+  end
+
+  private
+
+  def create_oauth_connection
+    Faraday.new(
+      url: 'https://oauth.vk.com',
+      params: {
+        client_id: ApplicationConfig['CLIENT_VK_ID'],
+        client_secret: ApplicationConfig['CLIENT_VK_SECRET'],
+        v: api_version
+      },
+      request: { timeout: 3 }
     )
   end
 end
