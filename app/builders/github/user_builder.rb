26ee8fd23d3ba549690c3aeb03a1d4fde1ace9f9
maxbarsukov <maximbarsukov@bk.ru>
2022-07-22 10:33:09 +0300

    feat: :sparkles: update Github user builder

--- app/builders/github/user_builder.rb
+++ app/builders/github/user_builder.rb
@@ -1,15 +1,17 @@
 class Github::UserBuilder
-  attr_reader :user_response
+  attr_reader :user_response, :params
 
   # @param [Response::Github::UserResponse] github_user_response
-  def initialize(github_user_response)
+  def initialize(github_user_response, params = {})
     @user_response = github_user_response
+    @params = params
   end
 
   # @return [User] user object without email
   def build
     User.new.tap do |user|
       user.username = user_response.login
+      user.email = params[:email]
 
       user.bio = user_response.bio
       user.blog = user_response.blog
