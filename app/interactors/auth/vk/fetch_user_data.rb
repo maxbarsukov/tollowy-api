2c8eb2fbbd2c6dec830a8dcf2d0930584d6038e8
maxbarsukov <maximbarsukov@bk.ru>
2022-07-22 16:59:58 +0300

    feat: :sparkles: fetch only current vk user data

--- app/interactors/auth/vk/fetch_user_data.rb
+++ app/interactors/auth/vk/fetch_user_data.rb
@@ -1,12 +1,10 @@
 class Auth::Vk::FetchUserData
   include Interactor
 
-  delegate :vk_access_token, :user_id, to: :context
+  delegate :vk_access_token, to: :context
 
   def call
-    return if context.existing_user
-
-    response = VkAdapter.new(vk_access_token).user_get(user_id)
+    response = VkAdapter.new(vk_access_token).user_get
     context.fail!(error_data: response.error_data) unless response.success?
 
     context.user_response = response
