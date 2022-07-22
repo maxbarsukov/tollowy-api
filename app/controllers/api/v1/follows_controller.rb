38390c92543765035e017719826f9792948bd720
maxbarsukov <maximbarsukov@bk.ru>
2022-07-22 15:12:10 +0300

    fix: :bug: fix followable id validating

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -38,7 +38,7 @@ class Api::V1::FollowsController < Api::V1::ApiController
   end
 
   def validate_followable_id!
-    return unless followable_type == 'User' && current_user.id.to_s == follow_params[:followable_id]
+    return if followable_type != 'User' || current_user.id.to_s != follow_params[:followable_id].to_s
 
     raise Params::InvalidParameterError, 'You cant follow yourself'
   end
