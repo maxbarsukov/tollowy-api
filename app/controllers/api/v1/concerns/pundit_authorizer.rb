2611cfe1c3d8512590d3285cd41207a224fd3a9f
maxbarsukov <maximbarsukov@bk.ru>
2022-07-26 16:09:36 +0300

    feat: :sparkles: add Pundit authorizer to raise custom error

--- app/controllers/api/v1/concerns/pundit_authorizer.rb
+++ app/controllers/api/v1/concerns/pundit_authorizer.rb
@@ -14,4 +14,20 @@ module Api::V1::Concerns::PunditAuthorizer
           record: args.first,
           policy: pundit_policy
   end
+
+  def authorize_with_error(record, query = nil, policy_class: nil)
+    query ||= "#{action_name}?"
+
+    record = Pundit.send(:pundit_model, record)
+    policy = policy_class.present? ? policy_class.new(current_user, record) : Pundit.policy!(current_user, record)
+
+    unless policy.public_send(query)
+      raise Auth::NotAuthorizedError,
+            error_code: policy&.error_code || :unauthorized,
+            message: policy.error_message,
+            query:, record:, policy:
+    end
+
+    record
+  end
 end
