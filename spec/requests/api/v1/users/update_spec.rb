06053e1f1148c783c74906e6e568b488dab07e24
maxbarsukov <maximbarsukov@bk.ru>
2022-07-29 11:48:39 +0300

    test: :rotating_light: add spec for user role update

--- spec/requests/api/v1/users/update_spec.rb
+++ spec/requests/api/v1/users/update_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'User Update', type: :request do
       context 'when email is valid' do
         context 'with changed email' do
           before do
-            user = create(:user, :with_user_role)
+            user = create(:user, :with_moderator_role)
             headers = { 'Authorization' => ApiHelper.authenticated_header(user:) }
             put "/api/v1/users/#{user.id}", headers:, params: {
               data: { type: 'user', attributes: { email: 'NewEmail1@mail.com' } }
@@ -19,6 +19,11 @@ RSpec.describe 'User Update', type: :request do
             expect(response).to have_http_status(:ok)
             data = JSON.parse(response.body)['data']
             expect(data['attributes']['email']).to eq('NewEmail1@mail.com')
+          end
+
+          it 'changes user role' do
+            expect(JSON.parse(response.body)['data']['attributes']['role']['name']).to eq('unconfirmed')
+            expect(User.last.role_before_reconfirm_value).to eq(Role.value_for(:moderator))
           end
 
           it 'adds a confirmation message' do
@@ -89,6 +94,95 @@ RSpec.describe 'User Update', type: :request do
 
           it 'does not send confirmation mail to user' do
             expect(ActionMailer::MailDeliveryJob).not_to have_been_enqueued
+          end
+        end
+      end
+    end
+
+    context 'with roles' do
+      context 'when roles is valid' do
+        before do
+          user = create(:user, :with_admin_role)
+          another_user = create(:user, :with_user_role)
+          headers = { 'Authorization' => ApiHelper.authenticated_header(user:) }
+          put "/api/v1/users/#{another_user.id}", headers:, params: {
+            data: { type: 'user', attributes: { role: 'premium' } }
+          }
+        end
+
+        it 'updates user role' do
+          expect(response).to have_http_status(:ok)
+          data = JSON.parse(response.body)['data']
+          expect(data['attributes']['role']['name']).to eq('premium')
+        end
+      end
+
+      context 'with invalid role' do
+        context 'when no such role' do
+          before do
+            user = create(:user, :with_admin_role)
+            another_user = create(:user, :with_user_role)
+            headers = { 'Authorization' => ApiHelper.authenticated_header(user:) }
+            put "/api/v1/users/#{another_user.id}", headers:, params: {
+              data: { type: 'user', attributes: { role: 'fake-role' } }
+            }
+          end
+
+          it 'fails with 404 not found' do
+            expect(response).to have_http_status(:not_found)
+            error = JSON.parse(response.body)['errors'][0]
+            expect(error['title']).to eq('Not Found')
+            expect(error['detail'][0]).to start_with('No such role: fake-role.')
+          end
+        end
+
+        context 'when user is not moderator' do
+          before do
+            user = create(:user, :with_user_role)
+            another_user = create(:user, :with_user_role)
+            headers = { 'Authorization' => ApiHelper.authenticated_header(user:) }
+            put "/api/v1/users/#{another_user.id}", headers:, params: {
+              data: { type: 'user', attributes: { role: 'banned' } }
+            }
+          end
+
+          it 'fails with 403 forbidden' do
+            expect(response).to have_http_status(:forbidden)
+            error = JSON.parse(response.body)['errors'][0]
+            expect(error['detail'][0]).to eq('User must be at least a moderator')
+          end
+        end
+
+        context 'when user is same' do
+          before do
+            user = create(:user, :with_admin_role)
+            headers = { 'Authorization' => ApiHelper.authenticated_header(user:) }
+            put "/api/v1/users/#{user.id}", headers:, params: {
+              data: { type: 'user', attributes: { role: 'banned' } }
+            }
+          end
+
+          it 'fails with 403 forbidden' do
+            expect(response).to have_http_status(:forbidden)
+            error = JSON.parse(response.body)['errors'][0]
+            expect(error['detail'][0]).to eq('You cant update your own role')
+          end
+        end
+
+        context 'when role is higher then yours' do
+          before do
+            user = create(:user, :with_moderator_role)
+            another_user = create(:user, :with_user_role)
+            headers = { 'Authorization' => ApiHelper.authenticated_header(user:) }
+            put "/api/v1/users/#{another_user.id}", headers:, params: {
+              data: { type: 'user', attributes: { role: 'admin' } }
+            }
+          end
+
+          it 'fails with 403 forbidden' do
+            expect(response).to have_http_status(:forbidden)
+            error = JSON.parse(response.body)['errors'][0]
+            expect(error['detail'][0]).to eq('You cant assign role higher then yours')
           end
         end
       end
