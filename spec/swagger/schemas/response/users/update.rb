69e46c1c4d1dcf9f5a78a5ef4fb980252ab0f7a3
maxbarsukov <maximbarsukov@bk.ru>
2022-07-29 11:14:20 +0300

    test: :rotating_light: update Schemas::Response::Users::Update schema

--- spec/swagger/schemas/response/users/update.rb
+++ spec/swagger/schemas/response/users/update.rb
@@ -1,1 +1,18 @@
-class Schemas::Response::Users::Update < Schemas::Response::Users::Show; end
+class Schemas::Response::Users::Update < Schemas::Base
+  def self.data
+    {
+      type: :object,
+      properties: {
+        data: Schemas::User.ref,
+        meta: {
+          type: :object,
+          properties: {
+            message: { type: :string }
+          },
+          required: %w[message]
+        }
+      },
+      required: %w[data meta]
+    }
+  end
+end
