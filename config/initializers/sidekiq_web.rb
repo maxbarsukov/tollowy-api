fe91982787fdb1c142f6fe4db75de8991c457f9e
maxbarsukov <maximbarsukov@bk.ru>
2022-07-31 12:26:17 +0300

    cut: :scissors: remove useless basic auth functionality for sidekiq web

--- config/initializers/sidekiq_web.rb
+++ config/initializers/sidekiq_web.rb
@@ -1,14 +1,2 @@
 Sidekiq::Web.use ActionDispatch::Cookies
 Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: '_interslice_session'
-
-if ENV.fetch('AUTH_USERNAME', nil) && ENV.fetch('AUTH_PASSWORD', nil) && Rails.env.production?
-  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    ActiveSupport::SecurityUtils.secure_compare(
-      ::Digest::SHA256.hexdigest(username),
-      ::Digest::SHA256.hexdigest(ENV.fetch('AUTH_USERNAME', nil))
-    ) & ActiveSupport::SecurityUtils.secure_compare(
-      ::Digest::SHA256.hexdigest(password),
-      ::Digest::SHA256.hexdigest(ENV.fetch('AUTH_PASSWORD', nil))
-    )
-  end
-end
