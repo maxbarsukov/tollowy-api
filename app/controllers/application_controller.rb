2727bfcb1c51aaf733193a1f7950f22a4c905a88
maxbarsukov <maximbarsukov@bk.ru>
2022-07-31 12:28:24 +0300

    style: :art: fix some rubocop offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_admin_user!
     return redirect_to admin_sign_in_path unless user_signed_in?
-    return if current_user.at_least_a?(:admin)
+    return if current_user.admin?
 
     reset_session
     redirect_to admin_sign_in_path, alert: I18n.t('admin.sessions.alert.you_are_not_an_admin')
