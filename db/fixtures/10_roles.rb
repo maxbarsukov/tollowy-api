87c44e7268448bdbc0bc17b140e9a33e0b7be752
maxbarsukov <maximbarsukov@bk.ru>
2022-07-31 10:28:38 +0300

    fix: :bug: fix roles seeding

--- db/fixtures/10_roles.rb
+++ db/fixtures/10_roles.rb
@@ -2,7 +2,7 @@ class RoleFixture < ApplicationFixture
   seed do
     roles = []
 
-    Role::ROLES.each_with_index do |role_name, ind|
+    Role::MAIN_ROLES.each_with_index do |role_name, ind|
       puts "#{ind}:\tRole(#{role_name})"
       roles << Role.new(name: role_name)
     end
