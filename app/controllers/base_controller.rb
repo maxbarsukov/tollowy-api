91fc82917a8430dae834487fe617fd26b6af096a
maxbarsukov <maximbarsukov@bk.ru>
2022-08-03 16:18:59 +0300

    feat: :sparkles: implement locale setting for api

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,4 +1,10 @@
 class BaseController < ActionController::API
   include Pagy::Backend
   include Pundit::Authorization
+
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
