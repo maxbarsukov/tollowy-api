fd539b663e663237de5dbbe1be263c97fc1c3c97
maxbarsukov <maximbarsukov@bk.ru>
2022-08-03 16:12:05 +0300

    fix: :bug: fix i18n fallback order

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Tollowy
 
     config.i18n.default_locale = :en
     config.i18n.available_locales = %i(en ru)
-    config.i18n.fallbacks = { en: :ru }
+    config.i18n.fallbacks = { ru: :en }
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
