c10bed3f2cd1c0598b6fd09928a13e1bafcd7ad0
maxbarsukov <maximbarsukov@bk.ru>
2022-08-04 14:40:13 +0300

    feat: :sparkles: implement IpAddress model

---
+++ app/models/ip_address.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: ip_addresses
+#
+#  id                 :bigint           not null, primary key
+#  blocked            :boolean          default(FALSE), not null
+#  ip                 :inet             not null
+#  user_sign_in_count :integer          default(0), not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  user_id            :bigint
+#
+# Indexes
+#
+#  index_ip_addresses_on_blocked  (blocked) WHERE blocked
+#  index_ip_addresses_on_ip       (ip) USING gist
+#  index_ip_addresses_on_user_id  (user_id)
+#
+class IpAddress < ApplicationRecord
+  belongs_to :user, optional: true
+
+  scope :blocked, -> { where('blocked') }
+
+  validates :ip, ip: true, presence: true
+  validates :user_sign_in_count, allow_nil: true, numericality: { only_integer: true }
+
+  def get_by_ip(ip)
+    IpAddress.find_by(['ip >>= inet ?', ip])
+  end
+end
